b684060a10ebce59f999d50b00765627cc261f62
Tan Quach <t@nqua.ch>
2021-12-24 21:12:26 +0000

    added script for installing tuxedo laptop drivers and apps

---
+++ scripts/tuxedo/install-tuxedo-apps.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+yay -S --noconfirm --needed tuxedo-backlight-control-git
+yay -S --noconfirm --needed tuxedo-keyboard
+yay -S --noconfirm --needed tuxedo-control-center-bin
+yay -S --noconfirm --needed tuxedo-touchpad-switch
+
