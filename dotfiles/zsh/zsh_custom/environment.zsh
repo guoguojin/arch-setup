9b401d8e4ebe2d0c89f63df97f723c3a56396cde
Tan Quach <t@nqua.ch>
2022-03-11 11:11:09 +0000

    Updated configs for hawkeye

--- dotfiles/zsh/zsh_custom/environment.zsh
+++ dotfiles/zsh/zsh_custom/environment.zsh
@@ -1,5 +1,5 @@
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-export GOPRIVATE=gitlab.com/alphaquants,gitlab.com/bl-go,gitlab.com/bl-alhambra,gitlab.com/bl-oakenfield,gitlab.com/bl-paladin,gitlab.com/birchwoodlangham
+export GOPRIVATE=gitlab.com/alphaquants,gitlab.com/bl-go,gitlab.com/bl-alhambra,gitlab.com/bl-oakenfield,gitlab.com/bl-paladin,gitlab.com/birchwoodlangham,code.vegaprotocol.io
 export DOCKER_BUILDKIT=1
 
