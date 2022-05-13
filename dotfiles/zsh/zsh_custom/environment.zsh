a0ec7a59048e9ea9685e2e8b3ea225062c6fa63d
Tan Quach <t@nqua.ch>
2022-05-13 08:53:21 +0100

    Update songbird config

--- dotfiles/zsh/zsh_custom/environment.zsh
+++ dotfiles/zsh/zsh_custom/environment.zsh
@@ -1,5 +1,6 @@
 export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
+export SOLANA_HOME=$HOME/.local/share/solana/install/active_release
+export PATH=$PATH:$GOPATH/bin:$SOLANA_HOME/bin
 export GOPRIVATE=gitlab.com/alphaquants,gitlab.com/bl-go,gitlab.com/bl-alhambra,gitlab.com/bl-oakenfield,gitlab.com/bl-paladin,gitlab.com/birchwoodlangham,gitlab.com/gobl
 export DOCKER_BUILDKIT=1
 
